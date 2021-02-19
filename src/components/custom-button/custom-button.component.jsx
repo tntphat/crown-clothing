91e00ff4d9d53ef4fda1a3083c2847d09e667549
Tấn Phát Tô Nguyễn <tonguyentanphat@gmail.com>
2021-02-19 15:39:17 +0700

    create cart-item, connect cart-dropdown

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-    <div className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <div className={`${inverted ? 'inverted' : ''} 
+        ${isGoogleSignIn ? 'google-sign-in' : ''} 
+    custom-button`}
     {...otherProps}>
         {children}
     </div>
