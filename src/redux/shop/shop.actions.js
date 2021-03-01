d95edd81e29c04d77960fa796d579241c9899553
Tấn Phát Tô Nguyễn <tonguyentanphat@gmail.com>
2021-03-01 16:23:41 +0700

    Asynchronous Redux with redux-thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,30 @@
 import ShopActionTypes from './shop.types';
+import { firestore, collectionsSnapshotToMap} from '../../firebase/firebase.utils';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 });
+
+export const fetchCollectionFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+  console.log('DTKLLLLLLLLLLLLLL');
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+    collectionRef.get()
+    .then( snapshot => {
+        const collectionsMap = collectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap))
+    })
+    .catch( error => dispatch(fetchCollectionFailure(error.message)))
+  }
+}
